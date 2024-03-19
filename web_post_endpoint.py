bc2d54a23db670cea8ff54fdce10f4156ee2c4c4
Wenjun Mao <mkof616@gmail.com>
2024-03-18 22:02:32 -0400

    add CORSMiddleware

--- web_post_endpoint.py
+++ web_post_endpoint.py
@@ -1,10 +1,19 @@
 import json
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import AutoModel, AutoTokenizer
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL_PATH = "./CharacterGLM-6B"
 TOKENIZER_PATH = MODEL_PATH
